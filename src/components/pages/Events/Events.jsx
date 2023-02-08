86a2fe5e6c62f84b0de3b34108f4d4d5c30f4e78
Sarang Sahebrao Gaidhani <saranggaidhani22@gmail.com>
2023-02-08 12:38:23 +0530

    First commit Event Page is created In Team.jsx, I corrected the name of Prateek sir In package.json, I added bootstrap

--- src/components/pages/Events/Events.jsx
+++ src/components/pages/Events/Events.jsx
@@ -1,9 +1,100 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import './Events.css';
+import { Card } from '@mui/material';
+
+import img_1 from '../../../assets/images/Induction Program/img_1.jpg';
+import API_1 from '../../../assets/images/API Event/API_1.jpeg';
+import UP_eve from '../../../assets/images/contact-1.jpeg';
 
 function Events() {
   return (
-    <div>Events</div>
+
+    <div className="container">
+      <h1 className="text-center">Our Successful Events</h1>
+      <br />
+      <div className="text-center">
+        <div class="row g-3">
+
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="card card-outline-primary shadow">
+              <img src={img_1} alt="img" class="card-img-top" />
+              <div class="card-body">
+                <h5 class="card-title">Induction Program</h5>
+                <p class="card-text">Nov 2022 <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, necessitatibus? Officia modi enim provident consequatur? Voluptatem, et nemo? Inventore, repellat!</p>
+                <button id="register" class="btn btn-outline-primary shadow" name="btn-send" >
+                  <a href="#" target="_blank">Explore</a>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-4">
+            <div className="card card-outline-primary shadow">
+              <img src={API_1} alt="img" class="card-img-top" />
+              <div class="card-body">
+                <h5 class="card-title">API Event</h5>
+                <p class="card-text">Dec 2022 <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, necessitatibus? Officia modi enim provident consequatur? Voluptatem, et nemo? Inventore, repellat!</p>
+                <button id="register" class="btn btn-outline-primary shadow" name="btn-send">
+                  <a href="#" target="_blank">Explore</a>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-4">
+              <div className="card card-outline-primary shadow">
+              <img src={UP_eve} alt="img" class="card-img-top" /> 
+                <div class="card-body">
+                  <h5 class="card-title">Upcoming Event</h5>
+                  <p class="card-text">Feb 2023 <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, necessitatibus? Officia modi enim provident consequatur? Voluptatem, et nemo? Inventore, repellat!</p>
+                  <button id="register" class="btn btn-outline-primary shadow" name="btn-send">
+                                     <a href="#" target="_blank">Explore</a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          <div className="col-12 col-md-6 col-lg-4">
+            <div class="card card-outline-primary shadow">
+              <img src={UP_eve} alt="img" class="card-img-top" />
+              <div class="card-body">
+                <h5 class="card-title">Upcoming Event</h5>
+                <p class="card-text">Feb 2023 <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, necessitatibus? Officia modi enim provident consequatur? Voluptatem, et nemo? Inventore, repellat!</p>
+                <button id="register" class="btn btn-outline-primary shadow" name="btn-send">
+                  <a href="#" target="_blank">Explore</a>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-4">
+            <div class="card card-outline-primary shadow">
+              <img src={UP_eve} alt="img" class="card-img-top" />
+              <div class="card-body">
+                <h5 class="card-title">Upcoming Event</h5>
+                <p class="card-text">Feb 2023 <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, necessitatibus? Officia modi enim provident consequatur? Voluptatem, et nemo? Inventore, repellat!</p>
+                <button id="register" class="btn btn-outline-primary shadow" name="btn-send">
+                  <a href="#" target="_blank">Explore</a>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-md-6 col-lg-4">
+            <div class="card card-outline-primary shadow">
+              <img src={UP_eve} alt="img" class="card-img-top" />
+              <div class="card-body">
+                <h5 class="card-title">Upcoming Event</h5>
+                <p class="card-text">Feb 2023 <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, necessitatibus? Officia modi enim provident consequatur? Voluptatem, et nemo? Inventore, repellat!</p>
+                <button id="register" class="btn btn-outline-primary shadow" name="btn-send">
+                  <a href="#" target="_blank">Explore</a>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
   )
 }
 
 export default Events
+
--- src/components/pages/team/Team.jsx
+++ src/components/pages/team/Team.jsx
@@ -22,7 +22,7 @@ function Team() {
 
         <Card
           image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-          name="Prattek sir"
+          name="Prateek sir"
           post="Head"
         />
 
